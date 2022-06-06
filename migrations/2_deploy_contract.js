2af45f75b21251ebfbe11849a66971358cc502e7
draglagarch <dralag@dragla.com>
2022-06-06 21:25:23 +0200

    test ok

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,5 @@
+var SToken = artifacts.require('SToken');
+
+module.exports = function (deployer) {
+  deployer.deploy(SToken);
+};
---
+++ test/s_token_test.js
@@ -0,0 +1,13 @@
+const SToken = artifacts.require("SToken");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("SToken", function (accounts) {
+  it("should assert true", async function () {
+    await SToken.deployed();
+    return assert.isTrue(true);
+  });
+});
