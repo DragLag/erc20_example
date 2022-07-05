bbcb38ff6e452e1489c0c7e08ae2c5ca4a8d663f
draglagarch <dralag@dragla.com>
2022-07-05 15:31:51 +0200

    string to byte32 convert

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,12 @@
 const SToken = artifacts.require("SToken");
 
 const Ballot = artifacts.require("Ballot");
+const ethers = require('ethers')
+const utils = ethers.utils
 
 module.exports = async function (deployer) {
   await  deployer.deploy(SToken);
-  await  deployer.deploy(Ballot,[]);
+  await  deployer.deploy(Ballot,[utils.formatBytes32String("increase"),utils.formatBytes32String("decrease")]);
+
 };
 
